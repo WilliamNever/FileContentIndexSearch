034a5b6a71099dbd6564ed6c0ca582acdfc23a53
William <willnever97@hotmail.com>
2024-04-22 15:20:31 +0800

    add some improvement

--- FileSearchByIndex/FileSearchByIndex.Core/Helper/ValidationHelper.cs
+++ FileSearchByIndex/FileSearchByIndex.Core/Helper/ValidationHelper.cs
@@ -8,18 +8,32 @@ namespace FileSearchByIndex.Core.Helper
         public static List<ValidationResult> ReflectValidateProperties<T>(this T? inm) where T : class
         {
             List<ValidationResult> results = new List<ValidationResult>();
-            if (inm == null || inm.GetType().Equals(typeof(string)) || inm.GetType().IsValueType)
+            var type = typeof(T);
+            if (inm == null || type.Equals(typeof(string)) || type.IsValueType)
                 return results;
 
-            var Properties = inm.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)
+            var Properties = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)
                 .Where(x => x.PropertyType.IsClass && !typeof(string).Equals(x.PropertyType)).ToList()
                 ;
             foreach (var prop in Properties)
             {
                 var childObject = prop.GetValue(inm);
+                /*
+                 * if childObject is null, (childObject is IList childObjects) is false.
+                 * do not need to make sure if the convert to objects is null.
+                 * */
                 if (childObject is IList childObjects)
                     foreach (var obj in childObjects)
                         results.AddRange(ReflectValidateProperties(obj));
+                else if (childObject is IEnumerable IEChildren)
+                {
+                    /*
+                     * for the IEnumerable<T>, it is not IList in some situation.
+                     */
+                    var ietor = IEChildren.GetEnumerator();
+                    while (ietor.MoveNext())
+                        results.AddRange(ReflectValidateProperties(ietor.Current));
+                }
                 else
                     results.AddRange(ReflectValidateProperties(childObject));
             }
